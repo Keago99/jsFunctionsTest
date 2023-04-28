31183dd9c191ab0d223e7b5b2b38c63704d0ab7d
Keago99 <keatabde@gmail.com>
2023-04-28 17:44:30 +0200

    added functions for testing, all work.

---
+++ functions.js
@@ -0,0 +1,19 @@
+function add7(num){
+    let newnum = num + 7;
+    return newnum;
+}
+
+function multiply(num1,num2){
+    multiNum = num1 * num2;
+    return multiNum;
+}
+
+function capitalize(word){
+    const capWord = word.charAt(0).toUpperCase() + word.slice(1);
+    return capWord;
+}
+
+function lastLetter(word){
+    const lastLetter = word.charAt(word.length - 1);
+    return lastLetter;
+}
